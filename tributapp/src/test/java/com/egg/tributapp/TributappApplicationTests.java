a7747bb69e24feb42409cc1c41e2fa35f57f6550
Santiago Tonin <santiagotonin@hotmail.com>
2022-11-09 15:50:29 -0300

    Inicio Proyecto

---
+++ tributapp/src/test/java/com/egg/tributapp/TributappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.tributapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TributappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
