e8b4ee2a2a416736a337e9e0c542a0c61b499631
Santiago Tonin <santiagotonin@hotmail.com>
2022-11-11 16:01:02 -0300

    cambio en el controlador

--- tributapp/src/main/java/com/egg/tributapp/controladores/desarrolladorControlador.java
+++ tributapp/src/main/java/com/egg/tributapp/controladores/desarrolladorControlador.java
@@ -4,8 +4,6 @@ import com.egg.tributapp.entidades.Desarrollador;
 import com.egg.tributapp.excepciones.MiException;
 import com.egg.tributapp.servicios.DesarrolladorServicio;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
