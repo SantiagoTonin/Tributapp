e4b4a778931cb7d6d7de14a06ede34c732869297
eduardotomasini <104108719+eduardotomasini@users.noreply.github.com>
2022-11-15 20:50:14 -0300

    cuarto commit

---
+++ tributapp/src/main/java/com/egg/tributapp/entidades/EmpresasIt.java
@@ -0,0 +1,28 @@
+
+package com.egg.tributapp.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmpresasIt {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    private String razonSocial;
+    private String direccion;
+    
+}
