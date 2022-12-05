168a23e8390322d65942f3dda41265b36ef210f6
Santiago Tonin <santiagotonin@hotmail.com>
2022-12-04 21:28:16 -0300

    el ultimo apaga la luz

--- tributapp/src/main/java/com/egg/tributapp/repositorios/ComentarioRepositorio.java
+++ tributapp/src/main/java/com/egg/tributapp/repositorios/ComentarioRepositorio.java
@@ -12,7 +12,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -22,13 +21,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ComentarioRepositorio extends JpaRepository<Comentario, String> {
 
-//    @Query("SELECT c FROM Comentario c WHERE c.nombre= :nombre")
-//    public Comentario buscarPorNombre(@Param("nombre") String nombre);
     @Query(value = "SELECT * FROM Comentario c WHERE c.contador_id LIKE %:id%",nativeQuery = true)
     public List<Comentario> buscarId(@Param("id") String nombre);
     
-//    @Query("SELECT c FROM Comentario c WHERE c.nombre LIKE %?1%")
-//    public Comentario buscarPorPalabra( String nombre);
-    
+
     
 }
